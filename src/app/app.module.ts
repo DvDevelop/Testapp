0d30d6cea49a38778c78414e1c3dceba9a8d8a44
David SC <dvdevelop24@gmail.com>
2019-06-20 15:53:17 -0500

    add some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { BodyComponent } from './Components/body/body.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BodyComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
